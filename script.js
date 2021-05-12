50eadb3ff35a4ec15ed62ca2cb2fd78776d6f2d5
NashVillarta <>
2021-05-12 22:42:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let radiusC = 0
+let radiusFindA = 0
+let cCircle = 0
+let aCircle = 0
+let diameterC = 0
+let diameterA = 0
+
+document.getElementById('button').addEventListener('click', calculatecircumference)
+
+function calculatecircumference () {
+  diameterC = document.getElementById('diameterc').value
+  diameterC = parseInt(diameterC)
+  radiusFindC = diameterC / 2
+  radiusFindC = parseInt(radiusFindC)
+  cCircle = 2 * Math.PI * radiusFindC
+  alert('Circumference is ' + cCircle + '. Radius = ' + radiusFindC + '. Press Ok to dissmiss alert.')
+}
+
+document.getElementById('buttontwo').addEventListener('click', calculatearea)
+
+function calculatearea () {
+  diameterA = document.getElementById('diametera').value
+  diameterA = parseInt(diameterA)
+  radiusFindA = diameterA / 2
+  radiusFindA = parseInt(radiusFindA)
+  aCircle = Math.PI * radiusFindA * radiusFindA
+  alert('Area is ' + aCircle + '. Radius = ' + radiusFindA + '. Press Ok to dissmiss alert.')
+}
